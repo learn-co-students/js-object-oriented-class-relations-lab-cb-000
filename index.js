7662d0c42a73893f387559287632130492a57cc3
Jen Poole <contact.vandepool@gmail.com>
2018-04-21 21:16:02 +0000

    Trip class initial

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ let store = { drivers: [], passengers: [], trips: [] };
 // new Driver() - initialized with a name; returns a JavaScript object that has attributes of id, and name
 let driverId = 0;
 class Driver {
-  constructor (id, name) {
+  constructor (name) {
     this.id = driverId++;
     this.name = name;
     store.drivers.push(this); // add object to store upon being initialized
@@ -21,8 +21,8 @@ class Driver {
 // A passenger has many trips, and has many drivers through trips.
 // new Passenger() - initialized with a name; returns a JavaScript object that has attributes of id, and name
 let passengerId = 0;
-class Passenger (
-  constructor (id, name) {
+class Passenger {
+  constructor (name) {
     this.id = passengerId++;
     this.name = name;
     store.passengers.push(this); // add object to store upon being initialized
@@ -30,12 +30,21 @@ class Passenger (
 
   // trips() - returns all of the trips that a passenger has taken
   // drivers() - returns all of the drivers that has taken a passenger on a trip
-)
+}
 
 
 // Trip class
-
 // A trip belongs to a driver and belongs to a passenger.
 // new Trip() - initialized with an instance of driver and an instance of passenger; returns a JavaScript that has attributes id, driverId, and passengerId
-// driver() - returns the driver associated with the trip
-// passenger() - returns the passenger associated with the trip
+let tripId = 0;
+class Trip {
+  constructor (driver, passenger) {
+    this.id = tripId++;
+    this.driverId = driver.id;
+    this.passengerId = passenger.id;
+    store.trips.push(this); // add object to store upon being initialized
+  }
+
+  // driver() - returns the driver associated with the trip
+  // passenger() - returns the passenger associated with the trip
+}
